e083cabe39b82c1c7c73fa45b7e1e9c0b79e8910
Fernando Antunes <f.antunes84@gmail.com>
2021-12-29 20:59:59 -0300

    Arrumando o .gitignore

--- config.py
+++
@@ -1,4 +0,0 @@
-#SECRET_DJANGO = '4wx%f)@)n)g&fs896txxry7m6v1lc)my*t-zwz-1$noi01g_-='
-SECRET_DJANGO = 'qCxrT^P#J$sYkO81Vm$@pRm1pbrE60EpogjsTWG4H1Fm2whieU'
-USUARIO = 'fernando'
-SENHA = '7IlXz8S^Te4*'
