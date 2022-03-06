29989187770b1f5a73bf5b1c01f83d87d5f58c4a
Fernando Antunes <f.antunes84@gmail.com>
2022-03-06 14:45:12 -0300

    API: Retornando relacionamentos como objetos literais ao invés de ids

--- api/serializers/atividade_serializer.py
+++ api/serializers/atividade_serializer.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
+
+from api.serializers.area_serializer import AreaSerializer
+from api.serializers.sub_area_serializer import SubAreaSerializer
 from atividades.models import Atividade
 
 
 class AtividadeSerializer(serializers.ModelSerializer):
+    area = AreaSerializer()
+    sub_area = SubAreaSerializer()
+
     class Meta:
         model = Atividade
         fields = ['id', 'data', 'area', 'sub_area', 'plataforma', 'pessoa', 'descricao', 'tempo', 'usuario']
--- api/serializers/sub_area_serializer.py
+++ api/serializers/sub_area_serializer.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
+
+from api.serializers.area_serializer import AreaSerializer
 from atividades.models import SubArea
 
 
 class SubAreaSerializer(serializers.ModelSerializer):
+    areas = AreaSerializer(many=True)
+
     class Meta:
         model = SubArea
         fields = ['id', 'nome', 'descricao', 'areas', 'usuario']
