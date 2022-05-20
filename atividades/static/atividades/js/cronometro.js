6aa747eb4bef89c207aedf706c32445a1b876be8
Fernando Antunes <f.antunes84@gmail.com>
2022-05-19 22:33:06 -0300

    JAVASCRIPT: Refaturação do contador para melhorar clareza e corrigir bug no botão de inicializar.

--- atividades/static/atividades/js/cronometro.js
+++ atividades/static/atividades/js/cronometro.js
@@ -1,74 +1,73 @@
-var mm = 0;
-var ss = 0;
-var status = 1;
+// Counter Logic Variables
+let counter,
+    time = 1000,
+    mm = 0,
+    ss = 0,
+    counterIsRunning = false;
 
-var tempo = 1000;
-var cron;
-
-function start(){
-    if(status == 1){
-        cron = setInterval(() => { timer(); }, tempo);
-        status = 0;
-        document.getElementById('start').innerText = 'Parar'
-    } else {
-        clearInterval(cron);
-        status = 1;
-        document.getElementById('start').innerText = 'Retomar'
-    }
-
-}
-
-function registrar(){
-    if(document.getElementById('id_tempo').value == ''){
-        if(ss < 10){
-            document.getElementById('id_tempo').value = mm;
-        } else {
-            document.getElementById('id_tempo').value = mm + 1;
-        }
-        zerar()
-    } else {
-        if(ss < 10){
-            document.getElementById('id_tempo').value = parseInt(document.getElementById('id_tempo').value) + mm;
-        } else {
-            document.getElementById('id_tempo').value = parseInt(document.getElementById('id_tempo').value) + mm + 1;
-        }
-        zerar()
-    }
-}
-
-function plus(){
-    mm = mm + 1
-
-    var format = (mm < 10 ? '0' + mm : mm) + ':' + (ss < 10 ? '0' + ss : ss);
-    document.getElementById('counter').innerText = format;
-}
-
-function minus(){
-    if(mm > 0){
-        mm = mm - 1
-
-        var format = (mm < 10 ? '0' + mm : mm) + ':' + (ss < 10 ? '0' + ss : ss);
-        document.getElementById('counter').innerText = format;
-    }
-}
-
-function zerar(){
-    clearInterval(cron);
-    mm = 0;
-    ss = 0;
-    status = 1;
-
-    document.getElementById('counter').innerText = '00:00';
-    document.getElementById('start').innerText = 'Iniciar'
-}
+// HTML Elements
+let counterDisplay = document.getElementById('counter-display'),
+    startButton = document.getElementById('start-button'),
+    timeInput = document.getElementById('id_tempo');
 
 function timer(){
     ss++;
-    if(ss == 60){
+    if(ss === 60){
         ss = 0;
         mm++;
     }
 
-    var format = (mm < 10 ? '0' + mm : mm) + ':' + (ss < 10 ? '0' + ss : ss);
-    document.getElementById('counter').innerText = format;
+    let format = `${(mm < 10 ? `0${mm}` : mm)}:${(ss < 10 ? `0${ss}` : ss)}`;
+    counterDisplay.innerHTML = format;
+}
+
+function runCounter() {
+    if(counterIsRunning) {
+        clearInterval(counter);
+        counterIsRunning = false;
+        startButton.value = 'Retomar';
+    } else {
+        counter = setInterval(() => timer(), time);
+        counterIsRunning = true;
+        startButton.value = 'Parar';
+    }
+
+}
+
+function resetCounter(){
+    clearInterval(counter);
+    mm = 0;
+    ss = 0;
+    counterIsRunning = false;
+
+    counterDisplay.innerHTML = '00:00';
+    startButton.value = 'Iniciar'
+}
+
+function addMinute(){
+    mm++;
+    let format = `${(mm < 10 ? `0${mm}` : mm)}:${(ss < 10 ? `0${ss}` : ss)}`;
+    counterDisplay.innerHTML = format;
+}
+
+function subtractMinute(){
+    if(mm > 0){
+        mm--;
+        let format = `${(mm < 10 ? `0${mm}` : mm)}:${(ss < 10 ? `0${ss}` : ss)}`;
+        counterDisplay.innerHTML = format;
+    }
+}
+
+function registerOnInput(){
+    if(timeInput.value == ''){
+        ss < 10 ? timeInput.value = mm : timeInput.value = mm + 1;
+        resetCounter()
+    } else {
+        if(ss < 10){
+            timeInput.value = parseInt(timeInput.value) + mm;
+        } else {
+            timeInput.value = parseInt(timeInput.value) + mm + 1;
+        }
+        resetCounter()
+    }
 }
