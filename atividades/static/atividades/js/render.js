42dd2498ef0741754a96c88ac02f13ece39e3c1f
Fernando Antunes <f.antunes84@gmail.com>
2022-07-05 16:37:38 -0300

    JAVASCRIPT: Botões update para area, subarea, plataforma e pessoas

--- atividades/static/atividades/js/render.js
+++ atividades/static/atividades/js/render.js
@@ -4,7 +4,11 @@ import * as components from './components.js';
 const areaSelect = document.querySelector('#id_area'),
     areaUpdate = document.querySelector('#update-area'),
     subareaSelect = document.querySelector('#id_subarea'),
-    subareaUpdate = document.querySelector('#update-subarea');
+    subareaUpdate = document.querySelector('#update-subarea'),
+    plataformaSelect = document.querySelector('#id_plataforma'),
+    plataformaUpdate = document.querySelector('#update-plataforma'),
+    pessoaSelect = document.querySelector('#id_pessoa'),
+    pessoaUpdate = document.querySelector('#update-pessoa');
 
 
 async function renderAreasSelect() {
@@ -16,21 +20,44 @@ async function renderAreasSelect() {
 
 async function renderSubareasSelect() {
     let areaId = areaSelect.selectedIndex;
-    const subareas = await services.getSubAreasArea(areaId);
+    const subareas = await services.getSubareasArea(areaId);
 
     subareaSelect.length = 0;
     components.renderOptions(subareas, subareaSelect.id);
 }
 
+async function renderPlataformasSelect() {
+    let areaId = areaSelect.selectedIndex;
+    const plataformas = await services.getPlataformasArea(areaId);
+    
+    plataformaSelect.length = 0;
+    components.renderOptions(plataformas, plataformaSelect.id);
+}
+
+
+async function renderPessoasSelect() {
+    let areaId = areaSelect.selectedIndex;
+    const pessoas = await services.getPessoasArea(areaId);
+
+    pessoaSelect.length = 0;
+    components.renderOptions(pessoas, pessoaSelect.id);
+}
+
 
 areaSelect.addEventListener('focusout', () => {
     renderSubareasSelect();
+    renderPlataformasSelect();
+    renderPessoasSelect();
 });
 
-areaUpdate.addEventListener('click', () => {
-    renderAreasSelect();
-});
 
-subareaUpdate.addEventListener('click', () => {
-    renderSubareasSelect();
-});
+areaUpdate.addEventListener('click', () => renderAreasSelect());
+
+
+subareaUpdate.addEventListener('click', () => renderSubareasSelect());
+
+
+plataformaUpdate.addEventListener('click', () => renderPlataformasSelect());
+
+
+pessoaUpdate.addEventListener('click', () => renderPessoasSelect());
--- atividades/static/atividades/js/services.js
+++ atividades/static/atividades/js/services.js
@@ -8,8 +8,18 @@ export async function getSubAreas() {
     return await response.json();
 }
 
-export async function getSubAreasArea(areaId) {
+export async function getSubareasArea(areaId) {
     let response = await fetch(`/api/areas/${areaId}/subareas/`);
+    return await response.json();
+}
+
+export async function getPlataformasArea(areaId) {
+    let response = await fetch(`/api/areas/${areaId}/plataformas/`);
+    return await response.json();
+}
+
+export async function getPessoasArea(areaId) {
+    let response = await fetch(`/api/areas/${areaId}/pessoas/`);
     return await response.json();
 }
 
